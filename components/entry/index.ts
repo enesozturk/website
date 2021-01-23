05a0b2d26e50f31b0cfbec48c4b34e1937a872eb
Enes Ozturk <enesozturk.d@gmail.com>
2021-01-23 21:37:19 +0300

    Fixes for some ui bugs

--- components/entry/index.ts
+++ components/entry/index.ts
@@ -1,5 +1,4 @@
 export { default as Entry } from './Entry'
 export { default as EntryGroup } from './EntryGroup'
 export { default as EntryGroupText } from './EntryGroupText'
-export { default as TextEntry } from './TextEntry'
 export { default as SeeOthers } from './SeeOthers'
