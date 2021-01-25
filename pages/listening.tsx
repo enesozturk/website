178dabbb0f18c90200018d4dbe5e48dbc71e96aa
Enes Ozturk <enesozturk.d@gmail.com>
2021-01-25 23:16:33 +0300

    Update listening page, create json data

--- pages/listening.tsx
+++ pages/listening.tsx
@@ -2,83 +2,41 @@ import Page from '@components/page'
 import { Spotify } from '@components/icons'
 import { Entry, EntryGroupText } from '../components/entry'
 
+import data from '../data/listening.json'
+
 const Projects = () => {
   return (
     <Page title="Listening" description="Collection of my playlists">
-      <EntryGroupText title="Favorite Albums" emphasized />
-      <Entry
-        target="blank"
-        title="Simulation Theory"
-        description="Muse"
-        href="https://open.spotify.com/album/5OZgDtx180ZZPMpm36J2zC"
-        image="https://i.scdn.co/image/ab67616d00001e024cb163c1d111f77307c842b6"
-        icon={<Spotify />}
-      />
-      <Entry
-        target="blank"
-        title="Mutsuz Parti"
-        description="Büyük Ev Ablukada"
-        href="https://open.spotify.com/album/1UBVA82yVbyoaBkVnWKhB8?si=j2xjDx7xSZ2wacKNBceWOA"
-        image="https://i.scdn.co/image/ab67616d00001e02d85bc4f2cfba519163aa0d9f"
-        icon={<Spotify />}
-      />
-      <Entry
-        target="blank"
-        title="Lafıma Gücenme"
-        description="Ari Barokas"
-        href="https://open.spotify.com/album/5NsIMUWwAn4BMDvwyArZz7"
-        image="https://i.scdn.co/image/ab67616d00001e021d26b3e7ea803059a6e4ffea"
-        icon={<Spotify />}
-      />
-      <Entry
-        target="blank"
-        title="Ludovico II"
-        description="Şanışer"
-        href="https://open.spotify.com/album/2bNlPIlKyhtYJIe7lHa6F6?si=W6d1MVBfQvCwgE5hUQtXVg"
-        image="https://i.scdn.co/image/ab67616d00001e02114c2ebb9903b8a121b41aa5"
-        icon={<Spotify />}
-      />
-      <Entry
-        target="blank"
-        title="İyi Niyetli Bir Gün"
-        description="Gece"
-        href="https://open.spotify.com/album/03F98O6rqq2Eie8kZzcCFX"
-        image="https://i.scdn.co/image/ab67616d00001e02a28c98aca6ca2fc8a05430df"
-        icon={<Spotify />}
-      />
-      <EntryGroupText title="My Playlists" emphasized />
-      <Entry
-        target="blank"
-        title="Relaxing"
-        description="Playlist - Enes Öztürk"
-        href="https://open.spotify.com/playlist/16xiQIS7MKuTbuOheHnnSA"
-        image="https://i.scdn.co/image/ab67706c0000bebb7f34f13ebab6652ce735dc4b"
-        icon={<Spotify />}
-      />
-      <Entry
-        target="blank"
-        title="Instrumental - Piano"
-        description="Playlist - Enes Öztürk"
-        href="https://open.spotify.com/playlist/2aifbSMuIGllmWMptLmGL5"
-        image="https://i.scdn.co/image/ab67706c0000bebb4230239b939282d2799bba92"
-        icon={<Spotify />}
-      />
-      <Entry
-        target="blank"
-        title="Oldies"
-        description="Playlist - Enes Öztürk"
-        href="https://open.spotify.com/playlist/7JtlpKCAtXURJfynbUODFz"
-        image="https://i.scdn.co/image/ab67706c0000bebb137db8d5b8a74fc86d775500"
-        icon={<Spotify />}
-      />
-      <Entry
-        target="blank"
-        title="Evening Rest"
-        description="Playlist - Enes Öztürk"
-        href="https://open.spotify.com/playlist/0rLLhxlZyqucYXVkTKbQal"
-        image="https://i.scdn.co/image/ab67706c0000bebbb72ebc5b4bb6ce933976e157"
-        icon={<Spotify />}
-      />
+      <EntryGroupText title="Favorite Albums" smallGap />
+      {data.albums.map((item, index) => {
+        return (
+          <Entry
+            key={index}
+            smallGap
+            target="blank"
+            title={item.title}
+            description={item.author}
+            href={item.link}
+            image={item.image}
+            icon={<Spotify />}
+          />
+        )
+      })}
+      <EntryGroupText title="My Playlists" smallGap />
+      {data.playlists.map((item, index) => {
+        return (
+          <Entry
+            key={index}
+            smallGap
+            target="blank"
+            title={item.title}
+            description={item.author}
+            href={item.link}
+            image={item.image}
+            icon={<Spotify />}
+          />
+        )
+      })}
     </Page>
   )
 }
