469a48de611088118f1e8a4c6d41a8b7406c7e53
Enes Ozturk <enesozturk.d@gmail.com>
2021-01-25 23:36:54 +0300

    Create data json for reading and projects pages

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -2,6 +2,8 @@ import { Github } from '@components/icons'
 import Page from '@components/page'
 import { Entry, EntryGroupText } from '../components/entry'
 
+import data from '../data/projects.json'
+
 const Projects = () => {
   return (
     <Page
@@ -10,78 +12,31 @@ const Projects = () => {
     >
       <article>
         <EntryGroupText title="Open Source" />
-        <Entry
-          target="blank"
-          title="React Native Swipeable Panel"
-          description="Zero dependency swipeable bottom panel for React Native"
-          href="https://github.com/enesozturk/rn-swipeable-panel"
-          icon={<Github />}
-        />
-        <Entry
-          target="blank"
-          title="React Native Hold Menu [Soon]"
-          description="Hold to open context menu for React Native powered by Reanimated 2"
-          href=""
-          icon={<Github />}
-        />
-        <Entry
-          target="blank"
-          title="hyper-rename-tab"
-          description="Hyper plugin to rename your tabs"
-          href="https://github.com/enesozturk/hyper-rename-tab"
-          icon={<Github />}
-        />
-        <Entry
-          target="blank"
-          title="deploy-next-to-x"
-          description="Guides to deploy your Next JS apps 🚀"
-          href="https://github.com/enesozturk/deploy-next-to-x"
-          icon={<Github />}
-        />
-        <Entry
-          target="blank"
-          title="Chatectron"
-          description="Web socket implementation with Socket.io & Electron & React JS 💬 🖥"
-          href="https://github.com/enesozturk/chatectron"
-          icon={<Github />}
-        />
+        {data.opensource.map((item, index) => {
+          return (
+            <Entry
+              key={index}
+              target="blank"
+              title={item.title}
+              description={item.description}
+              href={item.link}
+              icon={<Github />}
+            />
+          )
+        })}
+
         <EntryGroupText title="with Casemice" />
-        <Entry
-          target="blank"
-          title="Keapad"
-          description="Interactive presentation tool for pharma"
-          href="https://casemice.com/newave_portfolio/keapad/"
-        />
-        <Entry
-          target="blank"
-          title="Casequest"
-          description="Casequest is a case discussion and presentation application for Pharma companies to communicate with doctors"
-          href="https://casemice.com/newave_portfolio/casestory/"
-        />
-        <Entry
-          target="blank"
-          title="Graphy Online"
-          description="Visual training tool for pharma"
-          href="https://casemice.com/newave_portfolio/graphyonline/"
-        />
-        <Entry
-          target="blank"
-          title="Casemobile"
-          description="Case based doctor training and communication tool"
-          href="https://casemice.com/newave_portfolio/casemobile/"
-        />
-        <Entry
-          target="blank"
-          title="Compass"
-          description="Self-detailing tool that transforms guideline recommendation into a decision tree for better patient management"
-          href="https://casemice.com/newave_portfolio/compass/"
-        />
-        <Entry
-          target="blank"
-          title="Docmaze"
-          description="Case based doctor training and communication tool"
-          href="https://casemice.com/newave_portfolio/docmaze/"
-        />
+        {data.casemice.map((item, index) => {
+          return (
+            <Entry
+              key={index}
+              target="blank"
+              title={item.title}
+              description={item.description}
+              href={item.link}
+            />
+          )
+        })}
       </article>
     </Page>
   )
--- pages/reading.tsx
+++ pages/reading.tsx
@@ -2,61 +2,39 @@ import Page from '@components/page'
 import { Goodreads } from '@components/icons'
 import { Entry, EntryGroupText, SeeOthers } from '../components/entry'
 
+import data from '../data/reading.json'
+
 const Paragliding = () => {
   return (
     <Page title="Reading" description="Collection of books that I read">
       <article>
         <EntryGroupText title="Currently Reading" />
-        <Entry
-          target="blank"
-          title="1984"
-          description="George Orwell"
-          href="https://www.goodreads.com/book/show/40961427-1984"
-          icon={<Goodreads />}
-        />
+        {data.currently.map((item, index) => {
+          return (
+            <Entry
+              key={index}
+              target="blank"
+              title={item.title}
+              description={item.author}
+              href={item.link}
+              icon={<Goodreads />}
+            />
+          )
+        })}
+
         <EntryGroupText title="Favorite Books I Read" />
-        <Entry
-          target="blank"
-          title="Mutlu Beyin"
-          description="Loretta Graziano Breuning"
-          href="https://www.goodreads.com/review/show/2999368252?book_show_action=false&from_review_page=1"
-          icon={<Goodreads />}
-        />
-        <Entry
-          target="blank"
-          title="21. Yüzyıl İçin 21 Ders"
-          description="Yuval Noah Harari"
-          href="https://www.goodreads.com/book/show/41034731-21-y-zy-l-i-in-21-ders"
-          icon={<Goodreads />}
-        />
-        <Entry
-          target="blank"
-          title="Serenad"
-          description="Zulfu Livaneli"
-          href="https://www.goodreads.com/book/show/41034731-21-y-zy-l-i-in-21-ders"
-          icon={<Goodreads />}
-        />
-        <Entry
-          target="blank"
-          title="Cehenneme Övgü"
-          description="Gündüz Vassaf"
-          href="https://www.goodreads.com/book/show/6902351-cehenneme-vg"
-          icon={<Goodreads />}
-        />
-        <Entry
-          target="blank"
-          title="Emotional Intelligence"
-          description="Harvard Business Review"
-          href="https://www.goodreads.com/book/show/23258980-on-emotional-intelligence"
-          icon={<Goodreads />}
-        />
-        <Entry
-          target="blank"
-          title="The Power of Habit"
-          description="Charles Duhigg"
-          href="https://www.goodreads.com/book/show/12609433-the-power-of-habit"
-          icon={<Goodreads />}
-        />
+        {data.read.map((item, index) => {
+          return (
+            <Entry
+              key={index}
+              target="blank"
+              title={item.title}
+              description={item.author}
+              href={item.link}
+              icon={<Goodreads />}
+            />
+          )
+        })}
         <SeeOthers
           title="See Others on Goodreads"
           href="https://www.goodreads.com/user/show/102775572-enes-zt-rk"
